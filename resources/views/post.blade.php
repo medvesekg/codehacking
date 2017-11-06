a6a597746116132b64ec6c373460eb925ca18b8b
Gregor <gregor_medvesek@yahoo.com>
2017-11-06 23:35:54 +0100

    Added disqus

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -31,110 +31,27 @@
 
     <hr>
 
-    @if(Session::has('comment_message'))
-        {{session('comment_message')}}
-    @endif
+    <div id="disqus_thread"></div>
+    <script>
 
-    <!-- Blog Comments -->
+        /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        /*
+         var disqus_config = function () {
+         this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+         this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+         };
+         */
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://localhost-pyx6eikq5q.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
-    @if(Auth::check())
-    <!-- Comments Form -->
-    <div class="well">
-        <h4>Leave a Comment:</h4>
-
-        {!! Form::open(['method'=>'POST', 'action'=>'PostCommentsController@store']) !!}
-
-            <input type="hidden" name="post_id" value="{{$post->id}}">
-
-            <div class="form-group">
-                {!! Form::label('body', 'Body:') !!}
-                {!! Form::textarea('body', null, ['class'=>'form-control', 'rows'=>3]) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::submit('Submit comment', ['class'=>'btn btn-primary']) !!}
-            </div>
-        {!! Form::close() !!}
-
-    @endif
-    <hr>
-
-    <!-- Posted Comments -->
-
-    @if(count($comments) > 0)
-        @foreach($comments as $comment)
-            <!-- Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img height="45" class="media-object" src="{{Auth::user()->gravatar}}" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">{{$comment->author}}
-                        <small>{{$comment->created_at->diffForHumans()}}</small>
-                    </h4>
-                    {{$comment->body}}
-
-
-                    @if(count($comment->replies) > 0)
-
-                        @foreach($comment->replies as $reply)
-
-                            @if($reply->is_active == 1)
-
-
-
-                        <div class="nested-comment media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" height="30" src="{{$reply->photo}}" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">{{$reply->author}}
-                                    <small>{{$reply->created_at->diffForHumans()}}</small>
-                                </h4>
-                                {{$reply->body}}
-                            </div>
-
-                            <div class="comment-reply-container">
-
-                                <button class="toggle-reply btn btn-primary pull-right">Reply</button>
-
-                                <div class="comment-reply col-sm-10">
-
-                            {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply']) !!}
-                                <div class="form-group">
-                                    <input type="hidden" name="comment_id" value="{{$comment->id}}">
-                                    {!! Form::label('body', 'Body: ') !!}
-                                    {!! Form::textarea('body', null, ['class'=>'form-control', 'rows'=>5]) !!}
-                                </div>
-                                <div class="form-group">
-                                    {!! Form::submit('Reply', ['class'=>'btn btn-primary']) !!}
-                                </div>
-                            {!! Form::close() !!}
-                                </div>
-                        </div>
-
-                        </div>
-                            @endif
-                        @endforeach
-                    @endif
-                </div>
-            </div>
-        @endforeach
-    @endif
-    <!-- Comment -->
-
-    </div>
-
-    @section('scripts')
-        <script>
-
-            $('.comment-reply-container .toggle-reply').click(function() {
-
-                $(this).next().slideToggle('slow');
-
-            });
-
-        </script>
-    @stop
+    <script id="dsq-count-scr" src="//localhost-pyx6eikq5q.disqus.com/count.js" async></script>
 
 @stop
